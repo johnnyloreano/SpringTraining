262d637be317f9e57881dbaf0e268fa36ce0c7df
johnnyloreano <laureano.pedrojoao@gmail.com>
2020-09-26 14:17:21 -0300

    First commit!

---
+++ src/main/java/springdemo/VolleyballCoach.java
@@ -0,0 +1,10 @@
+package springdemo;
+
+public class VolleyballCoach implements  Coach{
+
+
+    @Override
+    public String getDailyWorkout() {
+        return "Do some jumps around your house!";
+    }
+}
